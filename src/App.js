e1a64b9bd1caf2e32442dbd3dcf08121ba62deca
oudedibbes <oude.dibbes@gmail.com>
2021-11-29 11:39:08 +0100

    First Title component added to AwesomeApp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   const myName = 'René';
-  const myCourse = 'React Bootcamp'
+  const myCourse = 'React Bootcamp';
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World, my name is { myName } </h1>
-        <p>i just started this { myCourse }</p>
+    <div className='App'>
+      <header className='App-header'>
+        <p>
+          <Title content='React Bootcamp' />
+        </p>
+        <h1>Hello World, my name is {myName} </h1>
+        <p>i just started this {myCourse}</p>
       </header>
     </div>
   );
