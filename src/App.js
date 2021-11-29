140214278100e3fed6cdb07a15335c7640b1ab85
oudedibbes <oude.dibbes@gmail.com>
2021-11-29 12:17:43 +0100

    First Pokemon component added to AwesomeApp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Title from './components/Title';
+import Pokemon from "./components/Pokemon"
 
 function App() {
   const myName = 'René';
@@ -9,6 +10,20 @@ function App() {
       <header className='App-header'>
         <p>
           <Title content='React Bootcamp' />
+          <Pokemon
+          name="Charizard"
+          weight={90}
+          awesome={true}
+          terrifying={false}
+          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
+        />
+        <Pokemon
+          name="Bulbasaur"
+          weight={6.9}
+          awesome={true}
+          terrifying={false}
+          abilities={["Overgrow", "Chlorophyll"]}
+        />
         </p>
         <h1>Hello World, my name is {myName} </h1>
         <p>i just started this {myCourse}</p>
