576a273d98063649753c9192afa4ce92dde7740f
oudedibbes <oude.dibbes@gmail.com>
2021-11-29 11:16:24 +0100

    First initial commit off AwesomeApp

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const myName = 'René';
+  const myCourse = 'React Bootcamp'
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World, my name is { myName } </h1>
+        <p>i just started this { myCourse }</p>
       </header>
     </div>
   );
